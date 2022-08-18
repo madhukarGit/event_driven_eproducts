a547f428c4612bbfbaa71aece4ec109cc40741c2
H Madhukar <hmk02330@gmail.com>
2022-08-18 19:40:14 +0530

    first commit

---
+++ src/main/java/com/blog/productsstore/ProductsEstoreApplication.java
@@ -0,0 +1,13 @@
+package com.blog.productsstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsEstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsEstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blog/productsstore/ProductsEstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.productsstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsEstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
